d475189951f694cabae9b485ca23f6858a6a8d2f
sambasiva19948-coder <sambasiva19948@gmail.com>
2026-06-25 23:43:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Luxora AI JavaScript
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Luxora AI Loaded Successfully 🚀");
+
+    const button = document.querySelector(".btn");
+
+    if(button){
+
+        button.addEventListener("click", () => {
+
+            alert("Welcome to Luxora AI 🚀");
+
+        });
+
+    }
+
+});
